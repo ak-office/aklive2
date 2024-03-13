43a4fc3140ea8aaa10d7a1e7a67e7a0efd31d2f2
ak-office <129983417+ak-office@users.noreply.github.com>
2024-03-13 16:31:17 +0530

    Update Astro_Cricket.js

--- v3/master/sports/Astro_Cricket.js
+++ v3/master/sports/Astro_Cricket.js
@@ -21,7 +21,7 @@
           {
 
             "type": "dash",
-            "file": "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default.mpd",
+            "file": "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default_primary.mpd",
             "drm": {
               "clearkey": {
                 "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
