46c49464ff0a25208e3b588f501839d396274631
ak-office <129983417+ak-office@users.noreply.github.com>
2024-03-22 00:49:45 +0530

    Update Astro_Cricket.js

--- v3/master/sports/Astro_Cricket.js
+++ v3/master/sports/Astro_Cricket.js
@@ -15,31 +15,27 @@
     },
 
   "playlist": [
-      {
-        "sources": [
-          null,
-          {
-
-            "type": "hls",
-            "file": "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default_primary.mpd",
-            "drm": {
-              "clearkey": {
-                "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
-                "key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
-              }
-            },
-            "label": "0"
-          }
-        ],
-        image: "/images/play-cover.jpg",
-        title: "Astro Cricket",
-        description: "Join Telegram Channel @AK_LIVE_OFFICE"
-      }
-    ],
+		{
+			"sources": [
+				{
+					"default": false,
+					"type": "mpd",
+					"file": "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default_primary.mpd",
+					"drm": {
+						"clearkey": {
+							"keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
+							"key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
+						}
+					},
+					"label": "1"
+				},
+			]
+		}
+	],
 
     "image": "/images/play-cover.jpg"
   }).on('ready', function () {
     // Set poster image for video element to avoid black background for audio-only programs.
     var video = document.getElementById(this.id).getElementsByTagName('video')[0];
     if (video) video.setAttribute('poster', this.getConfig().image);
-  }); 
+  });
